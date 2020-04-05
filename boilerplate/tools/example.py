76f870a08c2a728d8f7e2dab4ac833569c3225f1
rkuebler+1-at-791436068258 <qigK3lNFkeGeQ7hUwZQ/Dz3pXE9FTSHRRHM7Nqp7AaY=>
2020-04-05 19:23:07 +0200

    tests

--- boilerplate/tools/example.py
+++ boilerplate/tools/example.py
@@ -5,8 +5,27 @@ from boilerplate.timer import Timer
 logger = logging.getLogger(__name__)
 
 print(__name__)
+
+
 @Timer(name='sleeper', text='sleeper executed in {:.3f} seconds', logger=logger.info)
 def sleeper(t_sec=1):
+    """
+    Function to let a specific time pass.
+
+    :param t_sec:
+    :return: t_sec
+
+    >>> sleeper(1)
+    1
+    >>> sleeper(0.5)
+    0.4
+    """
     logger.debug(f'sleeping for {t_sec} seconds')
     time.sleep(t_sec)
     return t_sec
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,9 @@
 from .context import boilerplate
 # https://docs.python-guide.org/writing/structure/#test-suite
+import boilerplate.tools.example
+
+
+def test_sleeper():
+    parameter_in = 0.5
+    expected = 0.5
+    assert boilerplate.tools.example.sleeper(parameter_in) == expected
